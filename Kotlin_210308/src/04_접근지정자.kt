563f26dbf5d77db01a0f23d201c69612968e3d62
Chansik Yun <chansigi@gmail.com>
2021-03-08 13:50:46 +0900

    .

--- Kotlin_210308/src/04_접근지정자.kt
+++ Kotlin_210308/src/04_접근지정자.kt
@@ -9,7 +9,8 @@ package ex4
 // Kotlin
 //   private - internal - protected - public
 //  - protected: 더 이상 같은 패키지 내에서 접근이 불가능합니다.
-
+//  - internal: 다른 모듈에서 접근이 불가능합니다.
+//     : package의 이름을 알수 없는 형태로 변경해서 사용한다.
 
 // Android Studio
 //  - Project
@@ -21,7 +22,31 @@ class Car {
     internal var age: Int = 10
 }
 
+public class Foo
+internal class Goo
+
+// 같은 파일에서만 접근할 수 있습니다.
+private class Hoo
+
+// 전역 함수에서도 동일한 개념을 사용합니다.
+public fun foo() {
+
+}
+
+internal fun goo() {
+
+}
+
+private fun hoo() {
+
+}
+
+
+
 fun main() {
+    var h = Hoo()
+
+
     val car = Car()
     // car.name = "Bob"
 }
