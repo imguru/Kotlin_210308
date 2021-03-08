1bcc00e232ed1af8c852de9679fc9aff3518fe1f
Chansik Yun <chansigi@gmail.com>
2021-03-08 16:50:48 +0900

    .

--- Kotlin_210308/src/08_봉인클래스.kt
+++ Kotlin_210308/src/08_봉인클래스.kt
@@ -21,8 +21,36 @@ enum class Color(val r: Int, val g: Int, val b: Int) {
     }
 }
 
+// 3. Java:   switch-case
+//    Kotlin: switch-case 문이 존재하지 않습니다.
+//            => when expression 을 사용해야 합니다.
+
+// Statement(문): 결과값이 존재하지 않습니다.
+// Expression(식): 결과값이 존재합니다.
+
+
+fun getName(color: Color): String {
+    // when
+    /*
+    when (color) {
+        Color.RED -> return "Red"
+        Color.GREEN -> return "Green"
+        Color.ORANGE -> return "Orange"
+        Color.YELLOW -> return "Yellow"
+    }
+    */
+    return when (color) {
+        Color.RED -> "Red"
+        Color.GREEN -> "Green"
+        Color.ORANGE -> "Orange"
+        Color.YELLOW -> "Yellow"
+    }
+}
+
 fun main() {
     val color = Color.RED
     println(color.r)
     println(color.rgb())
+
+    println(getName(color))
 }
