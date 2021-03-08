2050d0d714a73e88f7a5134a568d0db7d9c427c5
Chansik Yun <chansigi@gmail.com>
2021-03-08 15:16:52 +0900

    .

---
+++ Kotlin_210308/src/07_클래스고급문법.kt
@@ -0,0 +1,43 @@
+// 07_클래스고급문법.kt
+package ex7
+
+// Interface
+//   => Interface는 구현을 가지지 않습니다. => 기본 구현을 가질 수 있습니다.
+//      'Interface'에 새로운 기능을 추가하는 것이 어렵다.
+
+//   =>  Java8 부터는 인터페이스에 기본 구현을 제공할 수 있습니다.
+//       default method / defender method
+
+interface Clickable {
+    fun click()
+
+    fun showOff() {
+        println("Clickable")
+    }
+}
+
+interface Focusable {
+    fun focus()
+
+    fun showOff() {
+        println("Focusable")
+    }
+}
+
+// class Button implements Clickable implements Focusable
+class Button : Clickable, Focusable {
+
+    override fun showOff() {
+        super<Focusable>.showOff()
+        super<Clickable>.showOff()
+    }
+    
+    override fun click() {
+        println("Button click")
+    }
+
+    override fun focus() {
+        println("Button focus")
+    }
+}
+
--- Kotlin_210308/src/com/lge/ex6/Sample.java
+++ Kotlin_210308/src/com/lge/ex6/Sample.java
@@ -2,6 +2,15 @@ package com.lge.ex6;
 
 import ex6.User;
 
+interface Clickable {
+    void click();
+
+    // Defender method
+    default void showOff() {
+        System.out.println("Clickable");
+    }
+}
+
 public class Sample {
     public static void main(String[] args) {
         // User user = new User("Tom", 100);
