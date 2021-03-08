6ee6bb67b483e5cb16e525da7efecfc0c592458c
Chansik Yun <chansigi@gmail.com>
2021-03-08 14:04:17 +0900

    .

---
+++ Kotlin_210308/src/com/lge/ex5/Sample.java
@@ -0,0 +1,94 @@
+package com.lge.ex5;
+
+/*
+class User {
+    private String name;
+    private String address;
+    private int age;
+    private int level;
+
+    public User(String name, String address, int age, int level) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.level = level;
+    }
+
+    public User(String name, String address) {
+        this(name, address, 0, 0);
+    }
+
+    public User(String name, String address, int age) {
+        this(name, address, age, 0);
+    }
+}
+*/
+
+
+// 2. Builder
+class User {
+    private String name;
+    private String address;
+    private int age;
+    private int level;
+
+    private User(Builder b) {
+        this.name = b.name;
+        this.address = b.address;
+        this.age = b.age;
+        this.level = b.level;
+    }
+
+    // Nested Class
+    public static class Builder {
+        private String name;
+        private String address;
+        private int age;
+        private int level;
+
+        public Builder(String name, String address) {
+            this.name = name;
+            this.address = address;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder level(int level) {
+            this.level = level;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+
+
+    }
+
+
+}
+
+
+public class Sample {
+    public static void main(String[] args) {
+        String n = "Tom";
+        String addr = "Suwon";
+        int age = 42;
+        int l = 10;
+
+        // 1. Telescoping Constructor - Design Pattern X
+        // User user1 = new User(n, addr, age, l);
+        // User user2 = new User(n, addr, age);
+        // User user3 = new User(n, addr);
+
+        // 2. Builder Pattern - Design Pattern O
+        User user = new User.Builder(n, addr)
+                .age(42)
+                .level(10)
+                .build();
+
+    }
+}
