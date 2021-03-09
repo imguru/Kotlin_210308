db8deaa845c157eb72f94a040c739d5f4772936a
Chansik Yun <chansigi@gmail.com>
2021-03-09 13:56:31 +0900

    .

--- Kotlin_210308/src/11_위임2.kt
+++ Kotlin_210308/src/11_위임2.kt
@@ -3,6 +3,7 @@
 
 package ex11_2
 
+import java.util.function.Predicate
 import kotlin.reflect.KProperty
 
 /*
@@ -24,7 +25,7 @@ interface ValueChanged<T> {
     fun onChangedValue(old: T, new: T)
 }
 
-class SampleDelegate<T>(var field: T, val listener: ValueChanged<T>? = null) {
+class SampleDelegate<T>(var field: T, val listener: ValueChanged<T>? = null, val predicate: Predicate<T>? = null) {
     // Getter
     operator fun getValue(thisRef: User, property: KProperty<*>): T {
         return field
@@ -33,6 +34,10 @@ class SampleDelegate<T>(var field: T, val listener: ValueChanged<T>? = null) {
     // Setter
     operator fun setValue(thisRef: User, property: KProperty<*>, value: T) {
         if (value == field) {
+            return
+        }
+
+        if (predicate != null && predicate.test(value).not()) {
             return
         }
 
@@ -45,18 +50,36 @@ class SampleDelegate<T>(var field: T, val listener: ValueChanged<T>? = null) {
 // 프로퍼티의 값의 변경이 발생하면, 수행되어야 하는 로직을 캡슐화하고 싶다.
 // 1. field 저장할 수 있도록
 // 2. 값이 변경이 발생할 때마다 알림을 받고 싶다.
+// 3. 프로퍼티의 값이 valid 하면 변경될 수 있도록 하고 싶다. - Predicate
 
 class User {
-    var name: String by SampleDelegate("Bob", listener = object: ValueChanged<String> {
+    var name: String by SampleDelegate("Bob", listener = object : ValueChanged<String> {
         override fun onChangedValue(old: String, new: String) {
             println("onChangedValue: $old -> $new")
         }
     })
-    // var age: Int by SampleDelegate()
+
+    var age: Int by SampleDelegate(0,
+        listener = object : ValueChanged<Int> {
+            override fun onChangedValue(old: Int, new: Int) {
+                println("onChangedValue: $old -> $new")
+            }
+        },
+        predicate = object : Predicate<Int> {
+            override fun test(t: Int): Boolean {
+                return t >= 0
+            }
+        }
+    )
 }
 
 fun main() {
     val user = User()
     user.name = "Tom"
+    user.age = 42
+
+    user.age = -1
+    user.age = 100
+
     println(user.name)
 }
