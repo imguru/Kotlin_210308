48776d64eaacf0c5c03c0780e554814e06e3eebc
Chansik Yun <chansigi@gmail.com>
2021-03-09 17:14:24 +0900

    .

--- Kotlin_210308/src/15_중위함수.kt
+++ Kotlin_210308/src/15_중위함수.kt
@@ -8,9 +8,20 @@ package ex15
 //   = Generic Argument - Any 취급됩니다.
 
 infix fun <K, V> K.pair(second: V) = Pair(this, second)
-// => 인자가 한개인 함수는 중위 함수가 될 수 있습니다.
+// => 인자가 한개인 '메소드' 는 중위 함수가 될 수 있습니다.
+
+class User {
+    infix fun move(x: Int) {
+
+    }
+}
+
+// DSL(Domain Specific Language)
 
 fun main() {
+    val user = User()
+    user.move(10)
+
     // 42.pair("xxx")
 
     // val pair1: Pair<String, Any> = "name".pair("Tom") // pair("name", "Tom")
@@ -25,3 +36,30 @@ fun main() {
 
     println(json)
 }
+
+// iOS
+//  UI: Storyboard(xml)
+//  BL: Swift
+
+// => iOS 13
+//   SwiftUI
+
+// SWT
+//   UI: Java
+//   BL: Java
+
+// Android
+//  - UI: xml
+//    B.L: Java + xml
+//  UI: Kotlin(DSL) => Jetpack Compose
+//  BL: Kotlin
+
+// Flutter
+
+
+
+
+
+
+
+
