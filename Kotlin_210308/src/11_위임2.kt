00a22ff6c8bd3407f1756786cfc333a5f337646e
Chansik Yun <chansigi@gmail.com>
2021-03-09 13:50:23 +0900

    .

--- Kotlin_210308/src/11_위임2.kt
+++ Kotlin_210308/src/11_위임2.kt
@@ -1,4 +1,3 @@
-
 // 11_위임2.kt
 // 프로퍼티 위임: 프로퍼티에 대한 getter / setter에 대한 호출을 다른 객체에게 위임한다.
 
@@ -6,6 +5,7 @@ package ex11_2
 
 import kotlin.reflect.KProperty
 
+/*
 class SampleDelegate {
     // Getter
     operator fun getValue(thisRef: User, property: KProperty<*>): String {
@@ -18,9 +18,41 @@ class SampleDelegate {
         println("setValue - $value")
     }
 }
+*/
+
+interface ValueChanged<T> {
+    fun onChangedValue(old: T, new: T)
+}
+
+class SampleDelegate<T>(var field: T, val listener: ValueChanged<T>? = null) {
+    // Getter
+    operator fun getValue(thisRef: User, property: KProperty<*>): T {
+        return field
+    }
+
+    // Setter
+    operator fun setValue(thisRef: User, property: KProperty<*>, value: T) {
+        if (value == field) {
+            return
+        }
+
+        val old = field
+        field = value
+        listener?.onChangedValue(old, value)
+    }
+}
+
+// 프로퍼티의 값의 변경이 발생하면, 수행되어야 하는 로직을 캡슐화하고 싶다.
+// 1. field 저장할 수 있도록
+// 2. 값이 변경이 발생할 때마다 알림을 받고 싶다.
 
 class User {
-    var name: String by SampleDelegate()
+    var name: String by SampleDelegate("Bob", listener = object: ValueChanged<String> {
+        override fun onChangedValue(old: String, new: String) {
+            println("onChangedValue: $old -> $new")
+        }
+    })
+    // var age: Int by SampleDelegate()
 }
 
 fun main() {
