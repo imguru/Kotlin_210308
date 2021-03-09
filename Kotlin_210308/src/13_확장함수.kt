492620d1be2c4c1828f199b4bd0e86c03757bceb
Chansik Yun <chansigi@gmail.com>
2021-03-09 15:50:17 +0900

    .

--- Kotlin_210308/src/13_확장함수.kt
+++ Kotlin_210308/src/13_확장함수.kt
@@ -1,12 +1,71 @@
 // 13_확장함수.kt
 package ex13
 
+/*
 class User {
+    // (Int, Int) -> Unit
+    //  => (User, Int, Int) -> Unit
     fun move(x: Int, y: Int) {
         println("User move")
+    }
+
+    // () -> Unit
+    // => (User) -> Unit
+    fun print() {
     }
 }
 
 fun main() {
-    val fn = User::move
+    val fn1: (User, Int, Int) -> Unit = User::move
+    val fn2: (User) -> Unit = User::print
+
+    val user = User()
+    // user.move(10, 20)  // move(user, 10, 20)
+
+    fn1(user, 10, 20)
+    fn2(user)
+
+    // 첫번째 인자가 객체로 결정되었습니다. - 바인드 된 참조(bound reference)
+    val fn3: (Int, Int) -> Unit = user::move
+    fn3(10, 20) // user.fn3(10, 20)
 }
+*/
+
+// Extension Function - 확장 함수
+// => 코틀린 라이브러리를 구현하는 핵심 기술
+
+// 기존 라이브러리를 확장하는 방법
+//  => 상속
+//    : 기반 클래스를 상속 받아서, 라이브러리를 확장해 사용한다.
+//    한계 - 기존 클래스가 상속을 목적으로 설계되어 있지 않다면, 확장이 불가능하다.
+//    - 수직 확장
+
+// => 확장 함수
+//    : 사용자가 정의한 함수가 기존 클래스의 메소드처럼 보이게 하는 기술
+//    - 수평 확장
+
+// fun lastChar(text: String): Char = text[text.length - 1]
+fun String.lastChar(): Char = this[length - 1]
+
+// String. - 수신 객체 타입
+// this    - 수신 객체 참조
+
+fun main() {
+    val str: String = "hello"
+    // val result = lastChar(str)
+    val result = str.lastChar()
+
+    // lastChar(str) -> str.lastChar()
+
+    println(result)
+}
+
+
+
+
+
+
+
+
+
+
