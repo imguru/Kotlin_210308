5c8793c566d5655bf9758c64c83f516a76c0a98b
Chansik Yun <chansigi@gmail.com>
2021-03-09 10:07:32 +0900

    .

--- Kotlin_210308/src/10_object.kt
+++ Kotlin_210308/src/10_object.kt
@@ -1,16 +1,36 @@
 // 10_object.kt
 package ex10
 
+import java.io.File
+import java.util.Comparator
+
 
 //  1. object declaration
 //   => Singleton을 생성할 때 사용하는 문법
 
 object Cursor {
-    fun move(x: Int, y: Int) {
+    var name: String = "Tom"
 
+    init {
+        name = "Bob"
+    }
+    // constructor 사용이 불가능합니다.
+
+    fun move(x: Int, y: Int) {
+    }
+}
+
+object CaseInsensitiveFileComparator : Comparator<File> {
+    override fun compare(o1: File, o2: File): Int {
+        return o1.path.compareTo(o2.path, ignoreCase = true)
     }
 }
 
 fun main() {
     Cursor.move(10, 20)
+
+    val files = listOf(File("/Z"), File("/a"))
+    val result = files.sortedWith(CaseInsensitiveFileComparator)
+
+    println(result)
 }
