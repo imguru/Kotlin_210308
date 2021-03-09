78b0a67fdd76e1c8fa7269b111ab9b9064a14029
Chansik Yun <chansigi@gmail.com>
2021-03-09 10:11:15 +0900

    .

--- Kotlin_210308/src/10_object.kt
+++ Kotlin_210308/src/10_object.kt
@@ -26,11 +26,24 @@ object CaseInsensitiveFileComparator : Comparator<File> {
     }
 }
 
+// 중첩 클래스 형태로도 사용할 수 있습니다.
+data class Person(val name: String) {
+    object NameComparator : Comparator<Person> {
+        override fun compare(o1: Person, o2: Person): Int {
+            return o1.name.compareTo(o2.name)
+        }
+    }
+}
+
+
 fun main() {
     Cursor.move(10, 20)
 
     val files = listOf(File("/Z"), File("/a"))
     val result = files.sortedWith(CaseInsensitiveFileComparator)
-
     println(result)
+
+    val people = listOf(Person("Bob"), Person("Alice"))
+    val result2 = people.sortedWith(Person.NameComparator)
+    println(result2)
 }
