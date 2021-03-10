0437e35f0b833424e8274705712052033ddc4b95
Chansik Yun <chansigi@gmail.com>
2021-03-10 16:58:30 +0900

    .

--- Kotlin_210308/src/22_메모이제이션2.kt
+++ Kotlin_210308/src/22_메모이제이션2.kt
@@ -1,4 +1,40 @@
 // 22_메모이제이션2.kt
 package ex22
 
+import com.google.gson.GsonBuilder
+
 // Gson - JSON Serialization
+data class User(val name: String, val age: Int)
+
+// JSON Serialization
+fun User.toJson(): String {
+    println("User.toJson()")
+
+    val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .create()
+
+    return gson.toJson(this)
+}
+
+// 기존 함수에 메모이제이션을 추가하는 함수
+fun <A, B> ((A) -> B).memoized(): (A) -> B {
+    val cache = mutableMapOf<A, B>()
+    return { k ->
+        cache.getOrPut(k) {
+            this(k)
+        }
+    }
+}
+
+fun main() {
+    val user1 = User("Tom", 42)
+    val user2 = User("Tom", 42)
+
+    val toJson = User::toJson.memoized()
+    println(toJson(user1))
+    println(toJson(user2))
+
+    // println(user1.toJson())
+    // println(user2.toJson())
+}
