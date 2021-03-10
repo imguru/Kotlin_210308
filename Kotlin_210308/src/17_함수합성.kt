e505d9ec4602be16bcee5600524b231e48acac40
Chansik Yun <chansigi@gmail.com>
2021-03-10 12:03:56 +0900

    .

--- Kotlin_210308/src/17_함수합성.kt
+++ Kotlin_210308/src/17_함수합성.kt
@@ -3,9 +3,24 @@ package ex17
 
 // f(x) -> y
 // g(y) -> z
+//--------------
 // h(x) -> z
 
 // x -> f(x)  -> y ->  g(y)  ->  z
+
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun add2(x: Int, y: Int): Int = x + y
+
+fun compose0(f: (String) -> Int, g: (Any) -> Int): (String) -> Int {
+    return { x ->
+        val y = f(x)
+        val z = g(y)
+        z
+    }
+}
 
 fun compose1(f: (String) -> Int, g: (Any) -> Int): (String) -> Int = { x ->
     val y = f(x)
@@ -21,6 +36,9 @@ fun compose2(f: (String) -> Int, g: (Any) -> Int): (String) -> Int = { x ->
 // g(B) -> C
 //-----------
 // h(A) -> C
+
+// A -> f(A) -> B - > g(B) -> C -> h(C) -> D
+// Java 8 - Stream API
 
 // 1. 일반화
 fun <A, B, C> compose3(f: (A) -> B, g: (B) -> C): (A) -> C = { x ->
