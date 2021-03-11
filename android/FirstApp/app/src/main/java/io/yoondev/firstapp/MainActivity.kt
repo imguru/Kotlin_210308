dec8eb3a5b86e3d7b788ae9be12bf2288cf04235
Chansik Yun <chansigi@gmail.com>
2021-03-11 15:13:41 +0900

    .

--- android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity.kt
+++ android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity.kt
@@ -1,14 +1,12 @@
 package io.yoondev.firstapp
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commitNow
