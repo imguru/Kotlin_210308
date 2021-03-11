cec301110eb33b14c7a5862f3030964995709b2b
Chansik Yun <chansigi@gmail.com>
2021-03-11 10:50:07 +0900

    .

--- Kotlin_210308/src/24_코틀린표준라이브러리2.kt
+++ Kotlin_210308/src/24_코틀린표준라이브러리2.kt
@@ -8,16 +8,16 @@ package ex24
 
 fun main() {
     // val list = listOf(1, 2, 3, 4, 5, 6)
-    val list = emptyList<Int>()
+    // val list = emptyList<Int>()
+    val list = listOf(1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
 
     // 첫번째 원소
     // val result1 = list[0]         // list.get(0)
     // println(result1)
 
-
     // 예외   /  Nullable
-    // first / firstOrNull
-    // last  / lastOrNull
+    // - first / firstOrNull
+    // - last  / lastOrNull
 
     // val result1 = list.first()
     val result1 = list.firstOrNull() ?: 0
@@ -26,4 +26,32 @@ fun main() {
     //    println(result1)
     // }
 
+    // - take: 데이터 추출 연산
+    // - drop: 요소를 제거한 결과를 받을 때
+    // val result2 = list.take(8)
+    val result2 = list.takeIf {
+       it.count() > 10
+    }
+    println(result2)
+
+    val result3 = list.drop(2)
+    println(result3)
+
+    // - distinct: 중복된 요소를 제거한 리스트를 얻을 때
+    val result4 = list.distinct()
+    println(result4)
+
+    var i = 0
+    val result5 = list.distinctBy { e ->
+        e + 10 + i++
+    }
+    println(result5)
 }
+
+
+
+
+
+
+
+
